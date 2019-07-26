123d107dd23b88c8ff8d8eaea3a761e28dc074f9
kamichidu <c.kamunagi@gmail.com>
2019-07-26 18:46:24 +0900

    add prompt

--- profile.ps1
+++ profile.ps1
@@ -1,1 +1,47 @@
 $env:Path = "C:\msys64\usr\bin;$env:Path"
+
+function Prompt {
+    $LastExitCode = $global:LastExitCode
+    try {
+        $cwd = $PWD.Path
+        $Host.UI.RawUI.WindowTitle = Split-Path -Leaf -Path $cwd
+
+        # color prompt
+        & {
+            Write-Host -NoNewline "`n"
+            Write-Host -NoNewline -ForegroundColor Green 'PS'
+            Write-Host -NoNewline ' '
+            $prefixes = @()
+            if ($env:GOPATH) {
+                $prefixes += @(@{Name='$GOPATH'; Value=$env:GOPATH})
+            }
+            $prefixes += @(@{Name='$USERPROFILE'; Value=$env:USERPROFILE})
+            $fallback = $true
+            foreach ($prefix in $prefixes) {
+                $shorten = $cwd -replace [regex]::Escape($prefix.Value), $prefix.Name
+                if ($cwd -ne $shorten) {
+                    Write-Host -NoNewline -ForegroundColor Yellow $shorten
+                    $fallback = $false
+                    break
+                }
+            }
+            if ($fallback) {
+                Write-Host -NoNewline -ForegroundColor Yellow $cwd
+            }
+            if (Get-Command -Name git -ErrorAction SilentlyContinue) {
+                $branch = git branch 2>$null |
+                    Where-Object { $_.StartsWith('* ') } |
+                    Select-Object -Property @{Name='Name'; Expression={ $_.Substring(2) }}
+                if ($branch -and $branch.Name -ne '') {
+                    Write-Host -NoNewline ' '
+                    Write-Host -NoNewline -ForegroundColor Cyan "($($branch.Name))"
+                }
+            }
+            Write-Host -NoNewline "`n"
+        }
+        return "> "
+    }
+    finally {
+        $global:LastExitCode = $LastExitCode
+    }
+}
